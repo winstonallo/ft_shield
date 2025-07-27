fac9d0c4307fa54dc27c283a8da33561f43c8085
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2025-07-27 13:57:28 +0200

    Add cleaner error handling pattern

--- src/main.c
+++ src/main.c
@@ -39,28 +39,32 @@ read_line(const int client_fd, char *const buf, size_t max_len) {
 
 int
 main(__attribute__((unused)) int ac, __attribute__((unused)) char **av, char **env) {
-    int server_fd, client_fd;
+    int server_fd = -1, client_fd = -1;
     struct sockaddr_in address = {.sin_family = AF_INET, .sin_addr.s_addr = INADDR_ANY, .sin_port = htons(3443)};
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) == -1) {
         fprintf(stderr, "Error binding to fd %d: %s", server_fd, strerror(errno));
+        goto server_error;
         return 1;
     }
     if (listen(server_fd, 1) == -1) {
         fprintf(stderr, "Error listening on server fd fd %d: %s", server_fd, strerror(errno));
+        goto server_error;
         return 1;
     }
 
     if ((client_fd = accept(server_fd, NULL, NULL)) == -1) {
         fprintf(stderr, "Error accepting client: %s\n", strerror(errno));
+        goto server_error;
         return 1;
     }
 
     while (1) {
         char buf[sizeof(PWD) + 1] = {0};
         if (read_line(client_fd, buf, sizeof(buf)) <= 0) {
+            goto client_error;
             return 1;
         }
 
@@ -78,14 +82,17 @@ main(__attribute__((unused)) int ac, __attribute__((unused)) char **av, char **e
 
     if (dup2(client_fd, 0) == -1) {
         fprintf(stderr, "Error duplicating stdin to client fd %d: %s\n", client_fd, strerror(errno));
+        goto client_error;
         return 1;
     }
     if (dup2(client_fd, 1) == -1) {
         fprintf(stderr, "Error duplicating stdout to client fd %d: %s\n", client_fd, strerror(errno));
+        goto client_error;
         return 1;
     }
     if (dup2(client_fd, 2) == -1) {
         fprintf(stderr, "Error duplicating stderr to client fd %d: %s\n", client_fd, strerror(errno));
+        goto client_error;
         return 1;
     }
 
@@ -96,4 +103,11 @@ main(__attribute__((unused)) int ac, __attribute__((unused)) char **av, char **e
     fprintf(stderr, "Could not execute %s: %s\n", bash_exec_path, strerror(errno));
 
     return 0;
+
+client_error:
+    close(client_fd);
+server_error:
+    close(server_fd);
+
+    return 1;
 }
