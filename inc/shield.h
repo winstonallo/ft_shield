a963ff3ad41843d309f3e1b4e8262231209a1824
winstonallo <arthurbiedchar@gmail.com>
2025-07-27 16:33:27 +0200

    Add template systemd config to shield.h

--- inc/shield.h
+++ inc/shield.h
@@ -1,4 +1,19 @@
 #ifndef SHIELD_H
 #define SHIELD_H
 
+#define SYSTEMD_CONFIG "[Unit]" \
+"Description=None of your business G" \
+"After=network.target" \
+"StartLimitIntervalSec=0" \
+ \
+"[Service]" \
+"Type=simple" \
+"Restart=always" \
+"RestartSec=1" \
+"User=root" \
+"ExecStart=%s" \
+\
+"[Install]" \
+"WantedBy=multi-user.target"
+
 #endif
