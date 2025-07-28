71a1ec0061c2a54decc7e04fefbbfe74780906ff
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2025-07-28 16:26:12 +0200

    Format shield.h

--- inc/shield.h
+++ inc/shield.h
@@ -5,22 +5,22 @@
 
 #define SYSTEMD_CONFIG_PATH "/etc/systemd/system/ft_shield.service"
 
-#define SYSTEMD_CONFIG "[Unit]\n" \
-"Description=None of your business G\n" \
-"After=network.target\n" \
-"StartLimitIntervalSec=0\n\n" \
- \
-"[Service]\n" \
-"Type=simple\n" \
-"Restart=always\n" \
-"RestartSec=1\n" \
-"User=root\n" \
-"ExecStart=/home/winstonallo/ft_shield/server\n\n" \
-\
-"[Install]\n" \
-"WantedBy=multi-user.target\n"
+#define SYSTEMD_CONFIG                                                                                                                                         \
+    "[Unit]\n"                                                                                                                                                 \
+    "Description=None of your business G\n"                                                                                                                    \
+    "After=network.target\n"                                                                                                                                   \
+    "StartLimitIntervalSec=0\n\n"                                                                                                                              \
+                                                                                                                                                               \
+    "[Service]\n"                                                                                                                                              \
+    "Type=simple\n"                                                                                                                                            \
+    "Restart=always\n"                                                                                                                                         \
+    "RestartSec=1\n"                                                                                                                                           \
+    "User=root\n"                                                                                                                                              \
+    "ExecStart=/bin/ft_shield\n\n"                                                                                                                             \
+                                                                                                                                                               \
+    "[Install]\n"                                                                                                                                              \
+    "WantedBy=multi-user.target\n"
 
-size_t __log(FILE* restrict __stream, const char* restrict __format, ...);
-
+size_t __log(FILE *restrict __stream, const char *restrict __format, ...);
 
 #endif
