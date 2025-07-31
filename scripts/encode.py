741081be553f912349b6c5ff694da730fdab9283
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2025-07-31 09:16:27 +0200

    Update scripts/encode.py to take a file path as an argument and escape \n

--- scripts/encode.py
+++ scripts/encode.py
@@ -1,6 +1,24 @@
+import re
 import sys
 
-if __name__ == "__main__":
-    s: str = sys.argv[1]
+
+def obfuscate_string(s: str, key=0x42) -> str:
+    res: list[str] = []
     for c in s:
-        print(f"'{c}' ^ 0x42,", end="")
+        if c == '\n': res.append(f"0x{0x0a ^ key:02x}")
+        else: res.append(f"0x{ord(c) ^ key:02x}")
+    res.append("0x00")
+    return "{" + ", ".join(res) + "}"
+
+def process_file(content: str) -> str:
+    def replace_obfuscate(match: re.Match) -> str:
+        string_content: str = match.group(1)
+        decoded_string: str = eval(f'"{string_content}"')
+        return obfuscate_string(decoded_string)
+    return re.sub(r'OBFUSCATE\("([^"]+)"\)', replace_obfuscate, content)
+
+if __name__ == "__main__":
+    with open(sys.argv[1], 'r') as f:
+        content: str = f.read()
+
+    print(process_file(content))
